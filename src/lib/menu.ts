480b274b9453c7c261e34c2c4fb19a19d8cbbc26
Eric Zhang <ezhangtang@gmail.com>
2026-06-13 00:17:10 -0400

    Pan grape-cake focus up 5%

--- src/lib/menu.ts
+++ src/lib/menu.ts
@@ -93,7 +93,7 @@ export const MENU: MenuItem[] = [
     allergens: ['gluten', 'eggs', 'dairy'],
     ingredients: ['jasmine sponge cake', 'green grapes', 'diplomat cream', 'heavy cream', 'jasmine tea'],
     image: '/menu/grape-cake.jpeg',
-    imagePosition: '70% 95%',
+    imagePosition: '70% 90%',
   },
   {
     id: 'strawberry-earl-grey-cookies',
